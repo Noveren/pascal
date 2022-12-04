0650a69b2d44c203781cbd4613232a6018efa30f
Noveren <1079148276@qq.com>
2022-12-04 22:22:29 +0800

    支持无括号四则运算

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -26,20 +26,19 @@ pub fn term<'a>() -> impl Parser<'a, Node> {
             factor(),
         ).m0()
     ).map(|(n1, vs)| {
-        let mut rev_iter = vs.into_iter().rev();
-        if let Some((op, node)) = rev_iter.next() {
-            let mut temp = (op, node);
-            while let Some((op, node)) = rev_iter.next() {
-                temp.1 = Node::Expr(
+        let mut iter = vs.into_iter();
+        if let Some((op, node)) = iter.next() {
+            let mut ret = Node::Expr(
+                if op == '*' { Symbol::MUL } else { Symbol::DIV },
+                vec![n1, node],
+            );
+            while let Some((op, node)) = iter.next() {
+                ret = Node::Expr(
                     if op == '*' { Symbol::MUL } else { Symbol::DIV },
-                    vec![node, temp.1],
+                    vec![ret, node],
                 );
-                temp.0 = op;
             }
-            Node::Expr(
-                if temp.0 == '*' { Symbol::MUL } else { Symbol::DIV },
-                vec![n1, temp.1],
-            )
+            ret
         } else {
             n1
         }
@@ -48,25 +47,32 @@ pub fn term<'a>() -> impl Parser<'a, Node> {
 
 #[allow(unused)]
 pub fn expr<'a>() -> impl Parser<'a, Node> {
-    term()
-    // pair(
-    //     number(),
-    //     pair(
-    //             right(whitespace::<false>(), the_char::<'+'>)
-    //         .or(right(whitespace::<false>(), the_char::<'-'>))
-    //         .or(right(whitespace::<false>(), the_char::<'*'>))
-    //         .or(right(whitespace::<false>(), the_char::<'/'>))
-    //         .map_err(|_| "Undefined Symbol".to_string()),
-    //         number(),
-    //     )
-    // ).map(|(n1, (op, n2))| Node::Expr(
-    //     match op {
-    //         '+' => Symbol::ADD,
-    //         '-' => Symbol::SUB,
-    //         '*' => Symbol::MUL,
-    //          _  => Symbol::DIV,
-    //     }, vec![n1, n2]
-    // ))
+    pair(
+        term(),
+        pair(
+                right(whitespace::<false>(), the_char::<'+'>)
+            .or(right(whitespace::<false>(), the_char::<'-'>))
+            .map_err(|_| "Undefined Symbol".to_string()),
+            term(),
+        ).m0()
+    ).map(|(n1, vs)| {
+        let mut iter = vs.into_iter();
+        if let Some((op, node)) = iter.next() {
+            let mut ret = Node::Expr(
+                if op == '+' { Symbol::ADD } else { Symbol::SUB },
+                vec![n1, node],
+            );
+            while let Some((op, node)) = iter.next() {
+                ret = Node::Expr(
+                    if op == '+' { Symbol::ADD } else { Symbol::SUB },
+                    vec![ret, node],
+                );
+            }
+            ret
+        } else {
+            n1
+        }
+    })
 }
 
 /// 执行 Block，记录并打印执行时间，然后返回 Block 的值
